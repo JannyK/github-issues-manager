e0faf42e66c37568b178b8e35388f2bb02c47b92
talk2tcheck <janny.kinende@gmail.com>
2015-03-23 22:02:02 +0100

    installed gradle build tool, created gradle wrapper

--- src/main/java/no/softeam/IssueController.java
+++ src/main/java/no/softeam/IssueController.java
@@ -1,7 +1,7 @@
 package no.softeam;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotypes.Controller;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
