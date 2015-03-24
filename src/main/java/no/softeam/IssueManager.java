9c99e2836d233a76a384b26ee45abbecd9a0a899
talk2tcheck <janny.kinende@gmail.com>
2015-03-24 22:43:03 +0100

    finished chap2

--- src/main/java/no/softeam/IssueManager.java
+++ src/main/java/no/softeam/IssueManager.java
@@ -3,28 +3,39 @@ package no.softeam;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.springframework.social.github.api.GithubIssue;
-import org.springframework.social.github.api.impl.GithubTemplate;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.social.github.api.GitHubIssue;
+import org.springframework.social.github.api.impl.GitHubTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
-public class IssueManager {
+public class IssueManager implements InitializingBean {
 
-	String githubToken = "49966bdc088e4471d576ecaf55d2673e0fb35016";
+	@Value("${github.token}")
+	String githubToken;
 
-	String org = "spring-projects";
-	String[] repos = new String[] {"spring-boot", "spring-boot-issues"};
+	@Value("${org}")
+	String org;
 
-	GithubTemplate githubTemplate = new GithubTemplate(githubToken);
+	@Value("${repos}")
+	String[] repos;
 
-	public List<Issue> findIÓpenIssues() {
+	GitHubTemplate githubTemplate;
+
+	@Override
+	public void afterPropertiesSet() throws Exception {
+		this.githubTemplate = new GitHubTemplate(githubToken);
+	}
+
+	public List<Issue> findOpenIssues() {
 		List<Issue> openIssues = new ArrayList<>();
 
 		for (String repo: repos) {
-			final List<GithubIssue> issues = githubTemplate
+			final List<GitHubIssue> issues = githubTemplate
 				.repoOperations().getIssues(org, repo);
 
-			for (GithubIssue issue: issues) {
+			for (GitHubIssue issue: issues) {
 				if (issue.getState().equals("open")) {
 					openIssues.add(new Issue(repo, issue));
 				}
